2194182176d702ab3197d714c9948a19f88632b2
unknown <yanspfc97@gmail.com>
2021-01-22 10:00:58 -0400

    Revert "updating host config again"
    This reverts commit 9b21d86bf47ba0175ebbae5c5f3a5e0d40970983.

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const connection = new Sequelize('heroku_03e3a9202585b1f', 'b41a015bd4e70e', '8384b291', {
-    host: '127.0.0.1:3306',
+    host: 'us-cdbr-east-03.cleardb.com',
     dialect: 'mysql'
 });
 
