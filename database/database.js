9dfebe7f5d72e12793d750660652048ca00540fc
unknown <yanspfc97@gmail.com>
2021-01-22 09:37:25 -0400

    updating host config

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const connection = new Sequelize('heroku_03e3a9202585b1f', 'b41a015bd4e70e', '8384b291', {
-    host: 'localhost',
+    host: 'us-cdbr-east-03.cleardb.com',
     dialect: 'mysql'
 });
 
